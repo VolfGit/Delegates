d8e0e5cc69551d8a6a4c818c540d20a947203d26
Your Name <you@example.com>
2024-02-20 23:13:13 +0200

    ne rabotaet

--- Delegats/Program.cs
+++ Delegats/Program.cs
@@ -22,10 +22,25 @@
 //с делегатами в C#. Если у вас есть вопросы или нужна дополнительная помощь,
 //не стесняйтесь спрашивать!
 
+
+//Создайте класс MathOperations, в котором определены методы для выполнения 
+//    математических операций: сложение, вычитание, умножение и деление.
+
+//Определите делегат, который может принимать несколько методов с сигнатурой 
+//int SomeOperation(int a, int b).
+
+//Создайте в классе Calculator метод, который принимает делегат и два числа. 
+//Этот метод должен последовательно вызывать методы, переданные в делегате,
+//используя переданные числа в качестве аргументов.
+
+//В методе Main программы создайте экземпляр класса MathOperations, 
+//экземпляр класса Calculator и создайте делегат, добавив в него несколько 
+//методов из MathOperations. Вызовите метод Calculator, передав этот делегат
+//и два числа в качестве аргументов, и выведите результат на консоль.
 public class Methoperations
 {
     public int TestPlus(int x, int y)
-    { 
+    {
         return x + y;
     }
     public int TestMinus(int x, int y)
@@ -40,13 +55,70 @@ public class Methoperations
     {
         return x / y;
     }
-}
 
+
+}
+public delegate int SomeOperation(int a,int b);
+public delegate int AdditionDelegate(int a, int b);
+public delegate int MinusDelegate(int a, int b);
+public delegate int MultiplyDelegate(int a, int b);
+public delegate int DivideDelegate(int a, int b);
+public class Calculator
+{
+    public int TestPlus1(int x, int y, AdditionDelegate op)
+    {
+        return op.Invoke(x, y);
+    }
+    public int TestMinus(int x, int y, MinusDelegate op)
+    {
+        return op.Invoke(x, y);
+    }
+    public int TestMultiply(int x, int y, MultiplyDelegate op)
+    {
+        return op.Invoke(x, y);
+    }
+    public int TestDivide(int x, int y, DivideDelegate op)
+    {
+        return op.Invoke(x, y);
+    }
+
+    public int TestDivide1(int x, int y, SomeOperation op)
+    {
+        return op.Invoke(x, y);
+
+    }
+    
+    public void AddDelegate(SomeOperation op,)
+    {
+        someOperation += multiplyDelegate;
+        someOperation += divideDelegate;
+        someOperation(3, 4);
+    }
+}
 public class Program
 {
     public static void Main(string[] args)
     {
+        Methoperations methoperations = new Methoperations();
+        Console.WriteLine(methoperations.TestMultiply(5, 2));
+        Console.WriteLine(methoperations.TestMinus(5, 2));
+        Console.WriteLine(methoperations.TestDivide(5, 2));
+        Console.WriteLine(methoperations.TestPlus(5, 2));
 
+        Calculator calculator = new Calculator();
+        AdditionDelegate additionDelegate = new AdditionDelegate(methoperations.TestPlus);
+        Console.WriteLine(calculator.TestPlus1(5, 2, additionDelegate));
+        DivideDelegate divideDelegate = new DivideDelegate(methoperations.TestDivide);
+        Console.WriteLine(calculator.TestDivide(5, 2, divideDelegate));
+        MultiplyDelegate multiply = new MultiplyDelegate(methoperations.TestMultiply);
+        Console.WriteLine(calculator.TestMultiply(10, 3, multiply));
+        MinusDelegate minusdelegate = new MinusDelegate(methoperations.TestMinus);
+        Console.WriteLine(calculator.TestMinus(10, 2, minusdelegate));
 
+        SomeOperation someOperation = new SomeOperation(methoperations.TestMultiply);
+        Calculator.AddDelegate(someOperation);
+
+        
+       
     }
 }
